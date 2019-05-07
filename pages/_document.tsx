ad1b30632a5176c67047f958816629f266da1687
John Apostol <notimetobeidle@gmail.com>
2019-05-06 23:36:25 -0500

    Create custom document

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  public static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  public render() {
+    return (
+      <Html>
+        <Head>
+          <style>{`body { margin: 0 } /* custom! */`}</style>
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ pages/post.tsx
@@ -0,0 +1,1 @@
+export default () => <div>Medium Post</div>;
