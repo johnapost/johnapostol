454ade174bf7489e43bf3eb6aead9b5a1cb78199
John Apostol <notimetobeidle@gmail.com>
2020-03-24 23:12:55 -0500

    Watch posts

--- dev-utils/exportPathMap.js
+++ dev-utils/exportPathMap.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
-const summary = require("./content/summary.json");
+const summary = require("../content/summary.json");
 
 module.exports = () => {
   const posts = Object.entries(summary.fileMap).reduce(
