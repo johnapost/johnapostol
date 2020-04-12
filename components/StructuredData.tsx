73f1a8f06b84a1d45cd97320fda80c40c1011e25
John Apostol <notimetobeidle@gmail.com>
2020-04-12 13:22:52 -0500

    Pass structured data tool

--- components/StructuredData.tsx
+++ components/StructuredData.tsx
@@ -11,13 +11,28 @@ const StructuredData = ({ date, title, hero, preview }: Props): JSX.Element => {
   const data = JSON.stringify({
     "@context": "https://schema.org",
     "@type": "BlogPosting",
+    publisher: {
+      "@type": "Organization",
+      name: "John Apostol",
+      url: "https://johnapostol.com",
+      logo: {
+        "@type": "ImageObject",
+        url: require("../public/static/me.jpg"),
+      },
+    },
     author: {
       "@type": "Person",
       name: "John Apostol",
+      url: "https://johnapostol.com",
+    },
+    mainEntityOfPage: {
+      "@type": "WebPage",
+      "@id": "https://google.com/article",
     },
     headline: title,
-    image: [hero],
+    image: hero,
     datePublished: date,
+    dateModified: date,
     description: preview,
   });
 
