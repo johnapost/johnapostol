995e8f6c0a4ef1a792247fb1f41ce0f036e59397
John Apostol <notimetobeidle@gmail.com>
2020-09-13 11:12:57 -0500

    Analytics prod (#151)
    * Move preconnect to GA component
    
    * Disallow GA on Cypress
    
    * Remove disallow for test
    
    * Disallow GA on Cypress again
    
    * Add Brave Creator verification

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const GoogleAnalytics = (): JSX.Element => (
   <>
+    <link rel="preconnect" href="https://www.google-analytics.com" />
     <script
       async
       src="https://www.googletagmanager.com/gtag/js?id=UA-139721576-1"
