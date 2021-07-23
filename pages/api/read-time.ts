2d50ba89e9cf71899f47812fe183431ee5f36b4e
John Apostol <notimetobeidle@gmail.com>
2021-07-22 21:58:00 -0500

    Complete initial draft of logic

--- pages/api/read-time.ts
+++ pages/api/read-time.ts
@@ -1,14 +1,15 @@
 import { gql } from "graphql-request";
 import { NextApiHandler } from "next";
 import requestCms from "../../utils/requestCms";
+import fetch from "isomorphic-unfetch";
 
-// const calcReadTime = (postBody: string): number => {
-//   // Remove new lines
-//   // Separate special markdown formatting (images)
-//   // Count length
-//   // Divide by 265 for readTime
-//   // Add wordcount substitute for images
-// };
+const calcReadTime = (postBody: string): number => {
+  // Remove new lines, count words, and get count of words
+  const words = postBody.replace(/\n/gm, "").trim().split(/\s+/).length;
+
+  // Divide by 265 for readTime
+  return words / 265;
+};
 
 const getPostBodyBySlug = async (slug: string): Promise<string> => {
   const data = gql`
@@ -25,21 +26,26 @@ const getPostBodyBySlug = async (slug: string): Promise<string> => {
   return postBody;
 };
 
-// const postReadTimeBySlug = async (slug: string, readTime: number) => {
-//   const mutation = gql`
-//     mutation {
-//       updatePost(
-//         where: { slug: "${slug}" }
-//         data: { readTime: ${readTime} }
-//       ) {
-//         readTime
-//       }
-//     }
-//   `;
+const postReadTimeBySlug = async (slug: string, readTime: number) => {
+  const mutation = gql`
+    mutation {
+      updatePost(
+        where: { slug: "${slug}" }
+        data: { readTime: ${readTime} }
+      ) {
+        readTime
+      }
+    }
+  `;
 
-//   const { post } = await requestCms(mutation);
-//   return post;
-// };
+  const { post } = await requestCms(mutation);
+  return post;
+};
+
+const triggerDeploy = async () =>
+  await fetch(process.env.VERCEL_DEPLOY_HOOK as string, {
+    method: "POST",
+  });
 
 const ReadTime: NextApiHandler = async (req, res) => {
   // Check the secret and query parameters
@@ -55,11 +61,9 @@ const ReadTime: NextApiHandler = async (req, res) => {
   // If the slug doesn't exist, return error
   if (!postBody) return res.status(401).json({ message: "Invalid slug" });
 
-  // const readTime = calcReadTime(postBody);
-  // await postReadTimeBySlug(req.query.slug as string, readTime);
-
-  // Trigger Vercel deploy
-  // TODO: Create secret for webhook
+  const readTime = calcReadTime(postBody);
+  await postReadTimeBySlug(req.query.slug as string, Math.ceil(readTime));
+  await triggerDeploy();
 
   // Close the response
   res.end();
